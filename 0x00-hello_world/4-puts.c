0e257652d84368884a75efba6e9fa09293b18f08
Etikerentse Samuel <setiks92@gmail.com>
2022-09-08 22:26:27 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - main block
- * Return: 0 
- */
+ * main - Entry Point
+ * Return: Always 0 (sucess)
+ */ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
