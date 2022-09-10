260b0f39598e4ae1f7a19499930075503a8eedfb
Etikerentse Samuel <setiks92@gmail.com>
2022-09-09 20:31:10 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,7 +24,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 not 0", n, ld);
+		printf("Last digit of %d is %d and is less than 6 and not 0", n, ld);
 	}
 	printf("\n");
 	return (0);
