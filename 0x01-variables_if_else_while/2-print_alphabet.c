784c0d59fc57c7cc647ef9886fb7adae229845af
Etikerentse Samuel <setiks92@gmail.com>
2022-09-09 20:45:51 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 'a'; a <= '2'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
