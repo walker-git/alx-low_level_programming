1b0eada2d5444d8228cf789cb619eeb440882bdc
Etikerentse Samuel <setiks92@gmail.com>
2022-09-09 22:07:43 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num, a;
+
+	for (num = '0'; num  <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
