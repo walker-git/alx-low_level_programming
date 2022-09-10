fa107a6fa31c1696807c44eb123b16693e4349f4
Etikerentse Samuel <setiks92@gmail.com>
2022-09-09 21:51:53 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num  <= '9'; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
