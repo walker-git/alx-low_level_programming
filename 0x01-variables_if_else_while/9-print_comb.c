0a63c3c85353583fed9e228b637b6c566d4faf94
Etikerentse Samuel <setiks92@gmail.com>
2022-09-11 14:06:24 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,17 +14,10 @@ int main(void)
 	for (num = '0'; num  <= '9'; num++)
 	{
 		putchar(num);
-		if (num != '9')
-		{
-
-		}
-		else
-		{
+		if (num == '9')
+			break;
 		putchar(',');
 		putchar(' ');
-		}
-	
 	}
 	putchar('\n');
 	return (0);
-}
