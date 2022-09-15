f4294d9542b343bf5a338f32ad69e37be3304dc3
Etikerentse Samuel <setiks92@gmail.com>
2022-09-15 14:39:47 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
 * main - causes an infinite loop
 * Return: 0
@@ -10,9 +9,9 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
-	/**
+
+	/*
 	*while (i < 10)
 	*{
 	*        putchar(i);
@@ -20,6 +19,5 @@ int main(void)
 	*/
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
