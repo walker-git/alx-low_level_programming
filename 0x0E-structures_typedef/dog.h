fc715bd7d6f2e83a725a118e79f6559abb81883f
Etikerentse Samuel <setiks92@gmail.com>
2022-10-11 14:56:49 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,21 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
+
+
 
 /**
- * struct dog - a dog's basic info
- * @name: First member
- * @age: Second member
- * @owner: Third member
- * Description: Longer description
+ * dog_t - An alais for struct dog
+ */
+
+typedef struct dog dog_t;
+
+
+
+/**
+ * struct dog - struct for dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  */
 
 struct dog
@@ -19,20 +28,9 @@ struct dog
 
 };
 
-
-/**
- * dog_t - typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-char *_strcpy(char *dest, char *src);
-int _strlen(char *s);
 
-
-
-#endif
+#endif /*_DOG_H_*/
