a53fd46dd227020bb7c59ccdb7fe8f58b2c75ebd
Etikerentse Samuel <setiks92@gmail.com>
2022-10-12 16:43:41 -0700

    This function prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,19 +1,15 @@
-#include <stdlib.h>
-#include "function_pointers.h"
-
 /**
  * print_name - prints a name
  * @name: name to print
- * @f: pointer to the printing function
+ * @f: callback function
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 
 {
 
-	if (!name || !f)
-
-	return;
+	if (name && f)
 
 	f(name);
 
