b7b80fef9424cc775029b97694b602a3f9225c40
Etikerentse Samuel <setiks92@gmail.com>
2022-12-16 18:28:35 -0800

    dynamic library task 0

--- 0x18-dynamic_libraries/main.c
+++ 0x18-dynamic_libraries/main.c
@@ -1,25 +1,13 @@
 #include "main.h"
 
-#include <stdio.h>
 
-
-
-/**
-
- * main - check the code
-
- *
-
- * Return: Always EXIT_SUCCESS.
-
- */
 
 int main(void)
 
 {
 
-    printf("%d\n", _strlen("My Dyn Lib"));
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 
-    return (EXIT_SUCCESS);
+    return (0);
 
 }
