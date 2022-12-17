298ea4bcc56f542e9be5c12a893aaf84b4aa1154
Etikerentse Samuel <setiks92@gmail.com>
2022-12-16 19:04:14 -0800

    task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -fPIC -c *.c
+gcc -Wall -Werror -Wextra -pedantic -C -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
